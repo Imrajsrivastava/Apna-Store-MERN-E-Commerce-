e03e753a68c9256169b96fcf1d56b4d933ef65e5
Imrajsrivastava <rajsri4485@gmail.com>
2023-07-06 13:59:21 +0530

    login-signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
 
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+
+// import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      {/* <LoginPage/> */}
+      {/* <SignupPage/> */}
+
+      <RouterProvider router={router} />
     </div>
   );
 }
